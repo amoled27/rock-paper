fbbf8370b8836656120d9e949415aa696e726116
amoled27 <amodshinde1995@gmail.com>
2021-10-22 16:33:53 +0530

    fix: logical and UI cahnges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useRef, useState } from "react";
 
 import rock from "./assets/rock.png";
@@ -8,13 +7,54 @@ import spock from "./assets/spock.png";
 import lizard from "./assets/lizard.png";
 import "./App.css";
 
+// selecting random number
+const randomizer = () => {
+  let max = 6;
+  let min = 1;
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+// wining conditions
+const checkWinner = (choice, userSelection) => {
+  if (userSelection === choice) return -1;
+  switch (choice) {
+    //rock
+    case 1: {
+      if (userSelection === 3 || userSelection === 5) return 1;
+      return 0;
+    }
+    //paper
+    case 2: {
+      if (userSelection === 1 || userSelection === 4) return 1;
+      return 0;
+    }
+    //scissors
+    case 3: {
+      if (userSelection === 2 || userSelection === 5) return 1;
+      return 0;
+    }
+    //spock
+    case 4: {
+      if (userSelection === 3 || userSelection === 1) return 1;
+      return 0;
+    }
+    //lizard
+    case 5: {
+      if (userSelection === 2 || userSelection === 4) return 1;
+      return 0;
+    }
+    default:
+      return 4;
+  }
+};
+
 function App() {
   // 0 players turn , 1 computer's turn
   const [turn, updateTurn] = useState(0);
-  const [currentSelection, updateSelect] = useState(0);
+  const [currentSelection, updateCurrentSelection] = useState(-1);
   const [compSelection, updateCompSelect] = useState(0);
   const [refArr, updateRefArr] = useState([]);
-  const [winnner, setWinner] = useState(2);
+  const [winner, setWinner] = useState(2);
 
   const rockRef = useRef(null);
   const paperRef = useRef(null);
@@ -26,18 +66,14 @@ function App() {
     updateRefArr([rockRef, paperRef, scissorsRef, spockRef, lizardRef]);
   }, []);
 
-  // selecting random number 
-  const randomizer = () => {
-    let max = 6;
-    let min = 1;
-    return Math.floor(Math.random() * (max - min) + min);
-  };
+  useEffect(() => {
+    const winner = checkWinner(compSelection, currentSelection);
+    setWinner(winner);
+  }, [compSelection, currentSelection]);
 
   // UI selection randomizer
   const randomUI = (count, rounds, stop) => {
     if (rounds < 0 && count === stop) {
-      const winner = checkWinner(stop);
-      setWinner(winner);
       updateCompSelect(stop);
       return;
     }
@@ -68,38 +104,6 @@ function App() {
     }, 300);
   };
 
-  // wining conditions
-  const checkWinner = (choice) => {
-    if (currentSelection === choice) return -1;
-    switch (choice) {
-      //rock
-      case 1: {
-        if (currentSelection === 3 || currentSelection === 5) return 1;
-        return 0;
-      }
-      //paper
-      case 2: {
-        if (currentSelection === 1 || currentSelection === 4) return 1;
-        return 0;
-      }
-      //scissors
-      case 3: {
-        if (currentSelection === 2 || currentSelection === 5) return 1;
-        return 0;
-      }
-      //spock
-      case 4: {
-        if (currentSelection === 3 || currentSelection === 1) return 1;
-        return 0;
-      }
-      //lizard
-      case 5: {
-        if (currentSelection === 2 || currentSelection === 4) return 1;
-        return 0;
-      }
-    }
-  };
-
   useEffect(() => {
     if (turn) {
       //computer's algo
@@ -113,9 +117,12 @@ function App() {
   // select choice handler
   const userSelection = (item) => {
     if (!turn) {
+      if (compSelection > 0)
+        refArr[compSelection - 1].current.style.filter = "invert(0)";
+      setWinner(2);
       updateCompSelect(0);
       updateTurn(1);
-      updateSelect(item);
+      updateCurrentSelection(item);
     }
   };
 
@@ -132,48 +139,54 @@ function App() {
         return "Spock";
       case 5:
         return "Lizard";
+      default:
+        return "None";
     }
   };
 
   // Rendering the results
   const renderWinner = () => {
-    switch (winnner) {
+    switch (winner) {
       case 0:
         return "You Won";
       case 1:
         return "Computer Won";
       case -1:
         return "DRAW ";
+      case 2:
+        return "Computer is Choosing...";
+      default:
+        return "Choose your Input";
     }
   };
 
   return (
     <div className="App">
-      <div className="game">
-        <h1>{winnner !== 2 ? renderWinner() : "Choose your Input"}</h1>
+      <div className="game row">
+        <h1>{renderWinner()}</h1>
         <div className="card-container">
           <div className="kard" ref={rockRef} onClick={() => userSelection(1)}>
-            <img className="img img-fluid" src={rock} />
+            <img className="img img-fluid" alt="rock" src={rock} />
           </div>
           <div className="kard" ref={paperRef} onClick={() => userSelection(2)}>
-            <img className="img img-fluid" src={paper} />
+            <img className="img img-fluid" alt="paper" src={paper} />
           </div>
           <div
             className="kard"
             ref={scissorsRef}
             onClick={() => userSelection(3)}
           >
-            <img className="img img-fluid" src={scissors} />
+            <img className="img img-fluid" alt="scissors" src={scissors} />
           </div>
           <div className="kard" ref={spockRef} onClick={() => userSelection(4)}>
-            <img className="img img-fluid" src={spock} />
+            <img className="img img-fluid" alt="spock" src={spock} />
           </div>
           <div
             className="kard"
             ref={lizardRef}
             onClick={() => userSelection(5)}
           >
-            <img className="img img-fluid" src={lizard} />
+            <img className="img img-fluid" alt="lizard" src={lizard} />
           </div>
         </div>
         <h2>
